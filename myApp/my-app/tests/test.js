fa77e38cdf1d239958f059da2cd38337abb050d3
Vladimir <gglobalink@gmail.com>
2020-05-06 01:05:06 +0300

    работающие тесты v1

---
+++ myApp/my-app/tests/test.js
@@ -0,0 +1,24 @@
+
+
+
+
+describe("nax", function() {
+
+    it("возводит в n-ю степень", function() {
+      assert.equal(pow(2, 3), 8);
+    });
+  
+  });
+
+  describe("pow", function() {
+
+    it("возводит в n-ю степень", function() {
+      assert.equal(pow(2, 3), 8);
+    });
+  
+  });
+ 
+
+  function pow() {
+    return 8; // :) мы - мошенники!
+  }
